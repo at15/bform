055c2a037537173237d3bf414787c5bcaba30d2c
unknown <at15@outlook.com>
2016-03-16 14:57:58 +0800

    Init project with spring boot
    - fullstack web
    - h2 (embedded)
    - elasticsearch

---
+++ src/main/java/cn/edu/sjtu/smse/BformApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.sjtu.smse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BformApplication.class, args);
+	}
+}
