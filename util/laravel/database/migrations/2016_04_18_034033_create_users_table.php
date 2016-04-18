812f38c1c20af8ad9055e21706f5f117eeb47424
at15 <at15@outlook.com>
2016-04-18 11:45:25 +0800

    Init laravel migrations #10
    I think I will just use the default laravel structure, don't want to
    add the --path parameters

---
+++ util/laravel/database/migrations/2016_04_18_034033_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        echo "day day up!";
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        echo "baby you are down down down";
+    }
+}
