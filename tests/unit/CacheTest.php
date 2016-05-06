a7e7b788d91e773679f183090a0f08616f1b6714
at15 <at15@outlook.com>
2016-05-06 19:47:21 +0800

    Init codeception #16
    - add autoload in bootstrap
    - add two dummy unit test

---
+++ tests/unit/CacheTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class CacheTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+        $this->assertEquals(true, true);
+    }
+
+    public function testMe2()
+    {
+        $this->assertEquals(true, true);
+    }
+}
